8cc895ead0264884bd1992f3f8c8dff7471108f6
Justin <justin@magneton-19.dynamic.rpi.edu>
2026-01-26 15:32:39 -0500

    Initial commit

---
+++ src/main/java/net/joosemann/lockslot/LockSlotClient.java
@@ -0,0 +1,10 @@
+package net.joosemann.lockslot;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LockSlotClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
