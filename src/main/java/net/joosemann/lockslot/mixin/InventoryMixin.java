d66a99181e8df1e566666905e3de13183ccb488b
JooseMann <justinkarate76@gmail.com>
2026-08-27 20:43:22 -0400

    Applied locked slot logic to item pickups.
    Added a mixin that makes sure a slot marked as "free" is also unlocked for the purposes of item pickups. This makes sure that an item pickup that would go into an empty locked slot will instead go into a different, unlocked slot if available (or not be picked up if no unlocked slot is available). With this, locked slots can be used as a tool for inventory management in addition preventing accidental item drops.

--- src/main/java/net/joosemann/lockslot/mixin/InventoryMixin.java
+++ src/main/java/net/joosemann/lockslot/mixin/InventoryMixin.java
@@ -1,6 +1,11 @@
 package net.joosemann.lockslot.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.joosemann.lockslot.data.LockedValues;
 import net.joosemann.lockslot.event.custom.ItemDropCallback;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -22,6 +27,10 @@ public abstract class InventoryMixin {
     @Shadow
     public abstract ItemStack getSelectedItem();
 
+    @Shadow
+    @Final
+    private NonNullList<ItemStack> items;
+
     // Lnet/minecraft/world/entity/player/Inventory;getSelectedItem()Lnet/minecraft/world/item/ItemStack;
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;getSelectedItem()Lnet/minecraft/world/item/ItemStack;"), method = "removeFromSelected", cancellable = true)
     private void onItemDrop(boolean bl, CallbackInfoReturnable<ItemStack> cir) {
@@ -32,4 +41,40 @@ public abstract class InventoryMixin {
         }
     }
 
+    // Prevent items from being picked up in an empty locked slot.
+    @Environment(EnvType.CLIENT)
+    @ModifyReturnValue(at = @At(value = "RETURN"), method = "getFreeSlot")
+    private int getNonLockedFreeSlot(int original) {
+
+        // If no slot was found originally, then we won't find one with stricter conditions.
+        // Return -1 to signal "no valid slot found".
+        if (original == -1) return -1;
+
+        // Get our row and column.
+        // The inventory measures indexes differently than our lock data: here, the hotbar is first, then the
+        // rest of the rows top-to-bottom. Our data goes top-to-bottom the whole way, so we need to adjust the row.
+        int row = original / 9 - 1; // - 1 shifts all rows but the hotbar (= -1) into the right spot
+        int col = original % 9;
+
+        // Then correct the hotbar case (row == -1 -> row = 3)
+        if (row == -1) row = 3;
+
+        // Check if this slot is unlocked, in which case we can use the original value.
+        if (!LockedValues.getLockedValue(row, col)) return original;
+
+        // Otherwise, this slot is locked, and we need to find another unlocked slot.
+        for (int i = original + 1; i < this.items.size(); ++i) {
+            // Update row and col
+            row = i >= 9 ? i / 9 - 1 : 3; // Same logic as above: i / 9 - 1 if i / 9 > 0, otherwise 3
+            col = i % 9;
+            // Find another slot is that both empty and unlocked
+            if (this.items.get(i).isEmpty() && !LockedValues.getLockedValue(row, col)) {
+                return i;
+            }
+        }
+
+        // We couldn't find any other unlocked slot, so prevent the item from being picked up at all.
+        // We do this by returning -1, signaling "no valid slot found".
+        return -1;
+    }
 }
